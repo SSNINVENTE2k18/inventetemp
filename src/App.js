c0652a59b7917bf9d3159ad6e0cf436c2d4e3ce0
ramakrishnar22 <34811582+ramakrishnar22@users.noreply.github.com>
2018-08-13 01:31:44 +0530

    Removed mobile number and added a link to goback to homepage

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
 
     return ( 
-         <div class="home">
+         <div className="home">
       <div id="homepage-div">
       <div id="cloud-div">
               <img src={cloud} id="cloud-css3" height="400px" width="200px" alt="newclouds"/></div>
@@ -41,7 +41,7 @@ class App extends Component {
        <img src={cloud} id="cloud-css" className="size-adjust" alt="newclouds"/>
       <img src={cloud} id="cloud-css1" className="size-adjust" alt="newclouds"/>
       <img src={cloud} id="cloud-css2" className="size-adjust" alt="newclouds"/>
-      <div id="notes-detail">Click on the center image to view the paper presentation</div>
+      <div id="notes-detail">Click on the center image to view the paper presentation details.</div>
      </div>
      </div>
     )
--- src/components/PaperPresentation.js
+++ src/components/PaperPresentation.js
@@ -1,23 +1,24 @@
 import React,{Component} from 'react';
-import {NavLink ,Route} from 'react-router-dom';
+import {NavLink ,Route,Redirect } from 'react-router-dom';
 import './PaperPresentation.css';
 import DataParser from './DataParser';
 class PaperPresentation extends Component{
             render(){
+                console.log(this.props);
                 const { match }=this.props;
                 console.log(match);
                 return (
                     <div id="pp-main-bg">
                       <div id="pp-main">
                           <div id="pp-content" className="flex-box-row">
-                               <div className="flex-hover"><NavLink className="link"  style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-IT`}><img  alt="it-imag" src={require('../assests/it.png')}/></NavLink></div>
-                               <div className="flex-hover"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-ECE`}><img alt="it-image2" src={require('../assests/ece.png')} /></NavLink></div>
-                               <div className="flex-hover"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CSE`}><img alt="it-image3" src={require('../assests/cse.png')} /></NavLink></div>
-                               <div className="flex-hover"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CIVIL`}><img alt="it-image4" src={require('../assests/civil.png')} /></NavLink></div>
-                               <div className="flex-hover"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-MECH`}><img alt="it-image5" src={require('../assests/mech.png')} /></NavLink></div>
-                               <div className="flex-hover"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-BIO`}><img alt="it-image6" src={require('../assests/bme.png')} /></NavLink></div>
-                               <div className="flex-hover"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-EEE`}><img alt="it-image7" src={require('../assests/eee.png')} /></NavLink></div>
-                               <div className="flex-hover"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CHEM`}><img alt="it-imag8e" src={require('../assests/chem.png')} /></NavLink></div>
+                               <div className="flex-hover" id="event-01"><NavLink className="link"  style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-IT`}><img  alt="it-imag" src={require('../assests/it.png')}/><div id="it-id">IT</div></NavLink></div>
+                               <div className="flex-hover" id="event-02"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-ECE`}><img alt="it-image2" src={require('../assests/ece.png')} /><div id="ece-id">ECE</div></NavLink></div>
+                               <div className="flex-hover" id="event-03"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CSE`}><img alt="it-image3" src={require('../assests/cse.png')} /><div id="cse-id">CSE</div></NavLink></div>
+                               <div className="flex-hover" id="event-04"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CIVIL`}><img alt="it-image4" src={require('../assests/civil.png')} /><div id="civil-id">CIVIL</div></NavLink></div>
+                               <div className="flex-hover" id="event-05"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-MECH`}><img alt="it-image5" src={require('../assests/mech.png')} /><div id="mech-id">MECH</div></NavLink></div>
+                               <div className="flex-hover" id="event-06"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-BIO`}><img alt="it-image6" src={require('../assests/bme.png')} /><div id="bme-id">BME</div></NavLink></div>
+                               <div className="flex-hover" id="event-07"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-EEE`}><img alt="it-image7" src={require('../assests/eee.png')} /><div id="eee-id">EEE</div></NavLink></div>
+                               <div className="flex-hover" id="event-08"><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CHEM`}><img alt="it-imag8e" src={require('../assests/chem.png')} /><div id="chem-id">CHEM</div></NavLink></div>
                             </div>
                             <div className="flex-box-row2" id="flex-box-row1">
                                 <div><NavLink className="link"  style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-IT`}>IT</NavLink></div>
@@ -25,11 +26,12 @@ class PaperPresentation extends Component{
                                 <div><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CSE`}>CSE</NavLink></div>
                                 <div><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CIVIL`}>CIVIL</NavLink></div>
                                 <div><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-MECH`}>MECH</NavLink></div>
-                                <div><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-BIO`}>BIO</NavLink></div>
+                                <div><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-BIO`}>BME</NavLink></div>
                                 <div><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-EEE`}>EEE</NavLink></div>
                                 <div><NavLink className="link" style={{ textDecoration: 'none', color:'white' }} to={`${match.url}/events-CHEM`}>CHEM</NavLink></div>
                             </div>
                        </div>
+                       <div id="home-page-redirect" ><NavLink to={{pathname:'/'}}  style={{ textDecoration: 'none', color:'white' }}>Click <strong>here!</strong> to go back to the Homepage</NavLink></div>
                        <Route path={`${match.url}/:name`} component={DataParser} />
                     </div>
                 )
